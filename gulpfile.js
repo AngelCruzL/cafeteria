84a350b65aea40a874ee1d4daaf261de5132921a
Ángel Cruz <langelcruzlara@gmail.com>
2021-09-05 22:37:11 -0500

    feat: adding functionality to generate webp image version

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+
 const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
 function compileCSS(done) {
   src('./src/scss/main.scss')
@@ -19,6 +21,10 @@ function images() {
     .pipe(dest('dist/img'));
 }
 
+function webpImages() {
+  return src('./src/img/**/*.{jpg,png}').pipe(webp()).pipe(dest('dist/img'));
+}
+
 function dev() {
   watch('./src/scss/**/*.scss', compileCSS);
   watch('./src/img/**/*', images);
@@ -26,5 +32,6 @@ function dev() {
 
 exports.compileCSS = compileCSS;
 exports.images = images;
+exports.webpImages = webpImages;
 exports.dev = dev;
-exports.default = series(images, compileCSS, dev);
+exports.default = series(images, webpImages, compileCSS, dev);
